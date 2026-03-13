8aca9f184a8e0ef653d0f519a223eb253f95ea21
Lucy <lu.anzoateguy@gmail.com>
2026-03-13 19:58:44 -0300

    feat: add login and register frontend

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from src import create_app
+from flask import send_from_directory
 
 app = create_app()
+
+@app.route('/frontend/<path:filename>')
+def frontend(filename):
+    return send_from_directory('frontend', filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
